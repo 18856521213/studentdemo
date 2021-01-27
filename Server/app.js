e72a8c2d0c46f61efde3190d3f3e406e405ffb7f
18856521213 <1332637611@qq.com>
2021-01-27 20:04:54 +0800

    测试提交

--- Server/app.js
+++ Server/app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({extended:false}));
 // app.use(express.urlencoded({ extended: false }))
 
 app.get('/home',(req,res)=>{
-    let name = {name:"dsd"}
+    let name = {names:"dsd"}
     res.json(name)
 })
 app.post('/time',(req,res)=>{
